83ba0d64a3de8b62337497a5d6575d8377b1fc9d
Danial Zamri <74848@siswa.unimas.my>
2025-12-28 19:05:32 +0800

    fix: update redirect URLs in auth and posts routes for improved navigation

--- routes/posts.js
+++ routes/posts.js
@@ -113,7 +113,7 @@ module.exports = function registerPostsRoutes(app, deps) {
       const slug = slugify(title)
       const spaPath = `/blog/${slug ? (slug + '-' + id) : id}`
       const requestPath = req.originalUrl || req.path || `/blog/${raw}`
-      const redirectUrl = `${protocol}://${host}${spaPath}`
+      const redirectUrl = `${protocol}://${host}/`
 
       const html = `<!doctype html>
 <html>
@@ -131,8 +131,8 @@ module.exports = function registerPostsRoutes(app, deps) {
     <meta name="twitter:title" content="${escapeHtml(title)}" />
     <meta name="twitter:description" content="${escapeHtml(description)}" />
     ${imageUrl ? `<meta name="twitter:image" content="${escapeHtml(imageUrl)}" />` : ''}
-    <link rel="canonical" href="${redirectUrl}" />
-    <script>setTimeout(()=>{window.location.href='${redirectUrl}'},100)</script>
+    <link rel="canonical" href="${protocol}://${host}${spaPath}" />
+    <script>window.location.replace('${redirectUrl}')</script>
   </head>
   <body>
   </body>
